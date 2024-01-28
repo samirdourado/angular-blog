76ea433157e7fac0ecd79757f65fe5b7b3b84877
samirdourado <samir3500@gmail.com>
2024-01-28 14:17:39 -0300

    feat: Created Header navs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
 import { BigCardComponent } from '../../components/big-card/big-card.component';
 import { SmallCardComponent } from '../../components/small-card/small-card.component';
+import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,7 @@ import { SmallCardComponent } from '../../components/small-card/small-card.compo
     MenuTitleComponent,
     BigCardComponent,
     SmallCardComponent,
+    MenuBarComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
